ea4f20ad1de56a2e3154f355ceb9feca2c27ee98
sdudiak <staszek.dudiak@irpark.pl>
2021-12-15 17:51:42 +0100

    finished IPackageQueue interface

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -5,20 +5,17 @@
 #include "package.hpp"
 class IPackageStockPile{
     public:
-
     using const_iterator = std::list<Package>::const_iterator;
-    
     virtual void push(const Package&& package);                            
     virtual bool empty() const;
     virtual std::size_t size() const;
     virtual ~IPackageStockPile();
 
-
-
-    
 };
 
 class IPackageQueue{
+virtual Package pop();
+virtual PackageQueueType get_queue_type() const; //PackageQueueType not implemented yet
 
 };
 
