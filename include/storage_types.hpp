f367c3402ac5d2f1ca2548d8d564ec5a995dcb5f
sdudiak <staszek.dudiak@irpark.pl>
2021-12-15 17:31:38 +0100

    added storage class templates

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -1,6 +1,18 @@
 #ifndef IMPLEMENTATION_STORAGE_TYPES_HPP
 #define IMPLEMENTATION_STORAGE_TYPES_HPP
 
+class IPackageStockPile{
+    public:
+    using const_iterator = std::list<Package>::const_iterator
+    
+}
 
+class IPackageQueue{
+
+}
+
+class PackageQueue{
+
+}
 
 #endif //IMPLEMENTATION_STORAGE_TYPES_HPP
