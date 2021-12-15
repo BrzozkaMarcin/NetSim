31a9da980a610ae78b900d093dda06d7d7bb6fd6
sdudiak <staszek.dudiak@irpark.pl>
2021-12-15 17:48:23 +0100

    finished IPackageStockPile interface

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -1,18 +1,29 @@
 #ifndef IMPLEMENTATION_STORAGE_TYPES_HPP
 #define IMPLEMENTATION_STORAGE_TYPES_HPP
 
+#include <list>
+#include "package.hpp"
 class IPackageStockPile{
     public:
-    using const_iterator = std::list<Package>::const_iterator
+
+    using const_iterator = std::list<Package>::const_iterator;
     
-}
+    virtual void push(const Package&& package);                            
+    virtual bool empty() const;
+    virtual std::size_t size() const;
+    virtual ~IPackageStockPile();
+
+
+
+    
+};
 
 class IPackageQueue{
 
-}
+};
 
 class PackageQueue{
 
-}
+};
 
 #endif //IMPLEMENTATION_STORAGE_TYPES_HPP
