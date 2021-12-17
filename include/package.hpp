f552b54117f5a3286413e3fd53714122c85ae62a
Marcin Brzózka <mbrzozka@student.agh.edu.pl>
2021-12-17 11:09:22 +0100

    Package

--- include/package.hpp
+++ include/package.hpp
@@ -1,15 +1,21 @@
 #ifndef IMPLEMENTATION_PACKAGE_HPP
 #define IMPLEMENTATION_PACKAGE_HPP
 
-#include <set>
 #include "types.hpp"
+#include <set>
 
 class Package {
 public:
-    Package();
+    explicit Package();
+    explicit Package(ElementID el);
+    Package(Package&& other);
+
+    Package& operator=(Package&& other);
+    ElementID get_id() const {return _element;}
+
     static std::set<ElementID> assigned_IDs;
     static std::set<ElementID> freed_IDs;
-    ElementID element;
+    ElementID _element{};
 
     ~Package();
 };
--- src/package.cpp
+++ src/package.cpp
@@ -1,20 +1,53 @@
 #include "package.hpp"
+#include <iostream>
 #include <set>
+#include <stdexcept>
 
 Package::Package() {
-    if (assigned_IDs.empty()) {
-        element = 1;
-        assigned_IDs.insert(element);
-    } else if (freed_IDs.empty() and ...) {
-        ...
+    if (assigned_IDs.empty() and freed_IDs.empty()) {
+        _element = 1;
+    } else if (not freed_IDs.empty()) {
+        _element = *freed_IDs.begin();
+        freed_IDs.erase(freed_IDs.begin());
+    } else {
+        _element = *(assigned_IDs.rbegin()) + 1;
     }
+    assigned_IDs.insert(_element);
+}
 
-};
+
+Package::Package(ElementID el) {
+    if (assigned_IDs.find(el) == assigned_IDs.end()) {
+        try {
+            throw std::invalid_argument("ID exists");
+        } catch (std::invalid_argument& ex) {
+            std::cout << ex.what() << std::endl;
+        }
+    }
+    else {
+        _element = el;
+        assigned_IDs.insert(_element);
+    }
+    if (freed_IDs.find(el) != freed_IDs.end()) {
+        freed_IDs.erase(el);
+    }
+}
+
+
+Package::Package(Package&& other) {
+    _element = other._element;
+}
+
+
+Package &Package::operator=(Package &&other) {
+    _element = other._element;
+    return *this;
+}
 
 
 Package::~Package() {
-    freed_IDs.insert(element);
-    assigned_IDs.erase(element);
-};
+    freed_IDs.insert(_element);
+    assigned_IDs.erase(_element);
+}
 
 
