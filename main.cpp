5f161a4978fc42fce4e47f6019b48f8b890f48b5
sdudiak <staszek.dudiak@irpark.pl>
2021-12-18 21:11:15 +0100

    added PackageQueue method definitions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "package.hpp"
-//#include "storage_types.hpp"
+#include "storage_types.hpp"
 #include "types.hpp"
 #include <iostream>
 #include <set>
@@ -14,8 +14,34 @@ int main() {
     Package pack6(Package(12));
     pack2.~Package();
     Package pack7;
+    // std::cout << pack1.get_id() << "\n" << pack2.get_id() << "\n" << pack3.get_id() << "\n";
+    // std::cout << pack4.get_id() << "\n" << pack5.get_id() << "\n" << pack6.get_id() << "\n" << pack7.get_id() << "\n";
 
-    std::cout << pack1.get_id() << "\n" << pack2.get_id() << "\n" << pack3.get_id() << "\n";
-    std::cout << pack4.get_id() << "\n" << pack5.get_id() << "\n" << pack6.get_id() << "\n" << pack7.get_id() << "\n";
+    PackageQueue fifo_queue(PackageQueueType::FIFO);
+    PackageQueue lifo_queue(PackageQueueType::LIFO);
+    std::cout<<fifo_queue.empty()<< "\n" << lifo_queue.empty() << "\n";
+    std::cout<<fifo_queue.size()<< "\n" << lifo_queue.size() << "\n";
+    fifo_queue.push(Package());
+    fifo_queue.push(Package());
+    fifo_queue.push(Package());
+    fifo_queue.push(Package());
+    fifo_queue.push(Package());
+
+    lifo_queue.push(Package());
+    lifo_queue.push(Package());
+    lifo_queue.push(Package());
+    lifo_queue.push(Package());
+    lifo_queue.push(Package());
+
+    
+    Package fifo_pop;
+    Package lifo_pop;
+    fifo_pop = fifo_queue.pop();
+    lifo_pop = lifo_queue.pop();
+
+
+
+
+
     return 0;
 }
--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,2 +1,36 @@
 #include "storage_types.hpp"
 
+
+void PackageQueue::push(Package&& other){
+queue_.emplace_back(std::move(other));
+}
+
+bool PackageQueue::empty(){
+    return queue_.empty();
+}
+
+PackageQueueType PackageQueue::get_queue_type(){
+    return queue_type_;
+}
+
+std::size_t PackageQueue::size(){
+    return queue_.size();
+}
+
+Package PackageQueue::pop(){
+    const_iterator it;
+    switch (queue_type_)
+    {
+    case PackageQueueType::FIFO:
+        it = end();
+        return *it;
+        queue_.pop_back();
+        break;
+    default:
+        it = begin();
+        return *it;
+        queue_.pop_front();
+        break;
+    }
+}
+
