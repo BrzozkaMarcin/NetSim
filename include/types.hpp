a3606c6ff3de27400ee86ce693cfacf84f53b45a
Marcin Brzózka <mbrzozka@student.agh.edu.pl>
2021-12-31 10:32:23 +0100

    v5

--- include/types.hpp
+++ include/types.hpp
@@ -1,6 +1,8 @@
 #ifndef IMPLEMENTATION_TYPES_HPP
 #define IMPLEMENTATION_TYPES_HPP
+#include <iostream>
 
-using ElementID = unsigned int;
+using ElementID = std::size_t;
+//using ElementID = unsigned int;
 
 #endif //IMPLEMENTATION_TYPES_HPP
