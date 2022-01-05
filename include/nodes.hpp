0905c90f5260117ee5202acd89da645c99a60809
KosoliC <adampekala48@gmail.com>
2022-01-05 17:20:05 +0100

    ipackagereceiver

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -5,5 +5,20 @@
 // Adam Pękala, nr 405380
 
 
+class IPackageReceiver {
+public:
+    virtual void receive_package(Package&& p) = 0;
+    virtual ElementID get_id() const = 0;
+    virtual ReceiverType get_receiver_type() const = 0;
+
+    using const_iterator = IPackageStockpile::const_iterator;
+
+    virtual const_iterator begin() const = 0;
+    virtual const_iterator cbegin() const = 0;
+    virtual const_iterator end() const = 0 ;
+    virtual const_iterator cend() const = 0;
+
+    virtual ~IPackageReceiver() = default;
+};
 
 #endif //IMPLEMENTATION_NODES_HPP
