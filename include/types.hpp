42b338a4837922783befa2b339384f1c25b40a27
Marcin Brzózka <mbrzozka@student.agh.edu.pl>
2022-01-05 14:55:01 +0100

    main_edit

--- include/types.hpp
+++ include/types.hpp
@@ -1,12 +1,12 @@
 #ifndef IMPLEMENTATION_TYPES_HPP
 #define IMPLEMENTATION_TYPES_HPP
-#include <iostream>
+//#include <iostream>
 
 // Marcin Brzózka, nr 405499
 // Stanisław Dudiak, nr 406903
 // Adam Pękala, nr 405380
 
-using ElementID = std::size_t;
-//using ElementID = unsigned int;
+//using ElementID = std::size_t;
+using ElementID = unsigned int;
 
 #endif //IMPLEMENTATION_TYPES_HPP
