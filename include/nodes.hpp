687f0d1821a70786417fdf33323b517dea583a46
sdudiak <staszek.dudiak@irpark.pl>
2022-01-05 17:21:05 +0100

    commit zeby adam nie zepsul kodu

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -3,7 +3,30 @@
 // Marcin Brzózka, nr 405499
 // Stanisław Dudiak, nr 406903
 // Adam Pękala, nr 405380
+#include <map> 
+class PackageSender{
+public:
+    RecieverPreferences reciever_preferences_;
+    void send_package();
+    
+};
 
+class RecieverPreferences{
+public:
+    RecieverPreferences(ProbabilityGenerator pg);
+    using preferences_t = std::map<IPackageReciever*,double>;
+    using const_iterator = preferences_t::const_iterator;
+    preferences_t preferences_;
+    const_iterator begin(){return preferences_.begin();}
+    const_iterator end(){return preferences_.end();}
+    const_iterator cbegin(){return preferences_.cbegin();}
+    const_iterator cend(){return preferences_.cend();}
+    void add_reciever(IpackageReciever* r);
+    void remove_reciever(IpackageReciever* r);
+    IpackageReciever* choose_reciever();
+    preferences_t& get_preferences()
+
+};
 
 
 #endif //IMPLEMENTATION_NODES_HPP
