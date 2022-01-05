24047ddb7e2f57ee590dc7990ca2a52fa3a4cde5
Marcin Brzózka <mbrzozka@student.agh.edu.pl>
2022-01-05 16:58:14 +0100

    factory1

--- include/factory.hpp
+++ include/factory.hpp
@@ -3,7 +3,21 @@
 // Marcin Brzózka, nr 405499
 // Stanisław Dudiak, nr 406903
 // Adam Pękala, nr 405380
+#include "nodes.hpp"
 
+enum class NodeColor {
+    UNVISITED, VISITED, VERIFIED
+};
+
+template <class Node>
+class NodeCollection{
+public:
+    using list = typename std::list<Node>;
+    using iterator = typename list::iterator;
+    using const_iterator = typename list::const_iterator;
+
+
+};
 
 
 #endif //IMPLEMENTATION_FACTORY_HPP
