abd8bc731c66302aa2f903d486b2fa182e4307a3
KosoliC <adampekala48@gmail.com>
2022-01-16 11:01:24 +0100

    Simulation cpp and hpp add.

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -4,6 +4,13 @@
 // Stanisław Dudiak, nr 406903
 // Adam Pękala, nr 405380
 
+#include "factory.hpp"
+#include "reports.hpp"
+#include <functional>
+#include "types.hpp"
+
+void simulate(Factory& f, TimeOffset d, std::function<void(Factory&, Time)> rf){
+}
 
 
 #endif //IMPLEMENTATION_SIMULATION_HPP
--- src/simulation.cpp
+++ src/simulation.cpp
@@ -3,3 +3,19 @@
 // Adam Pękala, nr 405380
 
 #include "simulation.hpp"
+#include <stdexcept>
+
+void simulate(Factory& f, TimeOffset d){
+    if(f.is_consistent()) {
+        for (Time i = 0; i <= d; i++ ) {
+            f.do_package_passing();
+            f.do_work(i);
+            f.do_deliveries(i);
+        }
+    }
+    else{
+            throw std::logic_error("Siec jest niespójna");
+    }
+}
+
+
